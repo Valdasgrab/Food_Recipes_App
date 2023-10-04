37234e4c2ecff5d18aabf520bba3bde4bed97718
vgrabauskas <valdasgrab@gmail.com>
2023-10-04 16:35:35 +0300

    Initial commit

---
+++ app/src/main/java/lt/vgrabauskas/foodrecipesapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package lt.vgrabauskas.foodrecipesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
