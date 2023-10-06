54e78427172ae454f66bb87771d4d9b7f705b622
vgrabauskas <valdasgrab@gmail.com>
2023-10-06 14:15:47 +0300

    Applied shimmer effect

--- app/src/main/java/lt/vgrabauskas/foodrecipesapp/RecipesFragment.kt
+++ app/src/main/java/lt/vgrabauskas/foodrecipesapp/RecipesFragment.kt
@@ -1,10 +1,15 @@
 package lt.vgrabauskas.foodrecipesapp
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewStub
+import android.widget.FrameLayout
+import com.facebook.shimmer.ShimmerFrameLayout
 
 class RecipesFragment : Fragment() {
 
@@ -12,7 +17,20 @@ class RecipesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        val shimmerPlaceholder = view.findViewById<ShimmerFrameLayout>(R.id.shimmer_placeholder_container)
+        val contentContainer = view.findViewById<ViewGroup>(R.id.content_container)
+
+        shimmerPlaceholder.startShimmer()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            shimmerPlaceholder.stopShimmer()
+            shimmerPlaceholder.visibility = View.GONE
+            val recipesRowLayout = inflater.inflate(R.layout.recipes_row_layout, null)
+            contentContainer.addView(recipesRowLayout)
+        }, 3000)
+
+        return view
     }
 }
